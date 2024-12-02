1acfd5dc5ae2a47aa58e9db40136050c2367abc8
Temitayo <akinstemi123@gmail.com>
2024-12-02 07:46:03 +0100

    first commit

---
+++ config/config.php
@@ -0,0 +1,26 @@
+<?php
+
+    try{
+
+
+        $host = "localhost";
+
+        $dbname = "jobboard";
+
+        $user = "root";
+
+        $pass = "";
+
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+
+    // if($conn == true){
+    //     echo "Cnnected Successfully";
+    // }else {
+    //     echo "error";
+    // }
